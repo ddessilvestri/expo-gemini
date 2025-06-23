a785bc32e28ce05f3da8865d012e91b7cf990c73
David Silvestri <dsilvestri@Mac.fibertel.com.ar>
2025-06-23 20:37:10 -0300

    enable front to fetch chunks of data and display in the app

--- store/basic-prompt/basicPrompt.store.ts
+++ store/basic-prompt/basicPrompt.store.ts
@@ -1,56 +1,57 @@
-
-import * as GeminiActions from '@/actions/gemini/basic-prompt.action';
-import { Message } from '@/interfaces/chat.interfaces';
 import uuid from 'react-native-uuid';
 import { create } from 'zustand';
 
+import * as GeminiActions from '@/actions/gemini';
+import { Message } from '@/interfaces/chat.interfaces';
+
 interface BasicPromptState {
-  geminiWriting: boolean,
-  messages: Message[],
-  addMessage: (text:string) => void;
-  setGeminiWriting: (isWriting:boolean) => void;
+  geminiWriting: boolean;
+  messages: Message[];
+  addMessage: (text: string) => void;
+  setGeminiWriting: (isWriting: boolean) => void;
 }
 
-const createMessage = (text:string,sender:'user' | 'gemini'): Message => {
-    return {  
-            id: uuid.v4(),
-            text: text,
-            createdAt: new Date(),
-            sender: sender,
-            type: 'text',    
-    };
-}
-
+const createMessage = (text: string, sender: 'user' | 'gemini'): Message => {
+  return {
+    id: uuid.v4(),
+    text: text,
+    createdAt: new Date(),
+    sender: sender,
+    type: 'text',
+  };
+};
 
 export const useBasicPromptStore = create<BasicPromptState>()((set) => ({
+  // State
+  geminiWriting: false,
+  messages: [],
 
-    //State
+  // Actions
+  addMessage: async (prompt: string) => {
+    const userMessage = createMessage(prompt, 'user');
+    const geminiMessage = createMessage('Generando respuesta...', 'gemini');
 
-    geminiWriting: false,
-    messages: [],
+    set((state) => ({
+      messages: [geminiMessage, userMessage, ...state.messages],
+    }));
 
+    // Petición a Gemini
 
-    //Actions
+    GeminiActions.getBasicPromptStream(prompt, (text) => {
+      set((state) => ({
+        messages: state.messages.map((msg) =>
+          msg.id === geminiMessage.id ? { ...msg, text } : msg
+        ),
+      }));
+    });
+    // const geminiMessage = createMessage(geminiResponseText, 'gemini');
 
-    addMessage: async (text:string) => {
-        const userMessage = createMessage(text,'user');
-        set( state => ({
-            geminiWriting:true,
-            messages: [userMessage, ...state.messages]
-        }) )
-
-        // Call gemini
-        const geminiResponseText = await GeminiActions.getBasicPrompt(text);
-        const geminiMessage = createMessage(geminiResponseText,'gemini');
-        
-        set( state => ({
-            geminiWriting:false,
-            messages: [geminiMessage, ...state.messages]
-        }) ) 
-    },
-
-    setGeminiWriting: (isWriting:boolean)=>{
-        set({geminiWriting: isWriting});
-    }
-
-}))
+    // set((state) => ({
+    //   geminiWriting: false,
+    //   messages: [geminiMessage, ...state.messages],
+    // }));
+  },
+  setGeminiWriting: (isWriting: boolean) => {
+    set({ geminiWriting: isWriting });
+  },
+}));
